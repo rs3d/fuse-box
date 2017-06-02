73c452b9c879f5f096dda77eb72f69f06676bf2e
Feras Khoursheed <ferasman@gmail.com>
2017-06-02 15:30:52 +0400

    Update modulesFolder Tests closes #83

--- src/tests/ModulesFolder.test.ts
+++ src/tests/ModulesFolder.test.ts
@@ -4,10 +4,12 @@ import {should} from "fuse-test-runner";
 
 export class ConfigTest {
     "Should throw if modulesFolder is the same as homeDir"() {
-        FuseBox.init({
-            homeDir: "C:/projects/Fuse-Box/fuse-box",
-            output: ".fusebox/test-bundle/$name.js",
-            modulesFolder: "C:/projects/Fuse-Box/fuse-box"
+        should().throwException(() => {
+            FuseBox.init({
+                homeDir: "C:/projects/Fuse-Box/fuse-box",
+                output: ".fusebox/test-bundle/$name.js",
+                modulesFolder: "C:/projects/Fuse-Box/fuse-box"
+            })
         })
     }
 
